3c90ac04f2fed4ae76728487436ed9c94cebfb43
Stygus <kuba.tokarczyk@onet.eu>
2024-10-24 21:30:52 +0100

    Dalsza implementacja klasy

--- ListaDwukierunkowa.cpp
+++ ListaDwukierunkowa.cpp
@@ -1,5 +1,9 @@
 ﻿#include <iostream>
 #include "ListaDwukierunkowa.h"
 
+Wezel::Wezel() : wartosc{}, poprzedni_wezel(nullptr), nastepny_wezel(nullptr) {  }  // konstruktor wezla z lista incicjalizacyjnom
 
+ListaDwukierunkowa::ListaDwukierunkowa() : poczatek(nullptr), koniec(nullptr), ilosc_elementow(0) {  }   // deklaracja konstruktora przypisanie wartości
+
+ListaDwukierunkowa::~ListaDwukierunkowa() { }
 
--- ListaDwukierunkowa.h
+++ ListaDwukierunkowa.h
@@ -1,9 +1,20 @@
 #pragma once
+struct Wezel
+{
+	int wartosc;
+	Wezel* poprzedni_wezel;
+	Wezel* nastepny_wezel;  // domyœlnie pola i metody s¹ publiczne w structcie 
+	Wezel();
+	~Wezel();
+};
+
+
 class ListaDwukierunkowa
 {
-
-
-
-
+	Wezel* poczatek;
+	Wezel* koniec;   // domyœlnie w clasie s¹ private
+	int ilosc_elementow;
+	ListaDwukierunkowa();
+	~ListaDwukierunkowa();
 };
 
